a87765bc67d8db8a6be4ef653855556275b7b73e
parush2305 <parush.p2305@gmail.com>
2020-09-14 10:16:35 +0530

    very very easy

--- practiseGithub.cpp
+++ practiseGithub.cpp
@@ -3,5 +3,12 @@ using namespace std;
 int main()
 {
     cout <<"practise github";
+    // to copy all
+    //- use git clone (address)
+    // to add changes
+    //-git add .
+    // -git commit -m "commit message "
+    //-git push origin master
+
     return 0;
 }
