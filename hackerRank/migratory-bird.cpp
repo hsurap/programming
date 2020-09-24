36d3ffb65497b4fa9491b672d2f388c110ec992f
parush2305 <parush.p2305@gmail.com>
2020-09-24 09:42:02 +0530

    medium

---
+++ hackerRank/migratory-bird.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,a1=0,a2=0,a3=0,a4=0,a5=0;
+    cin >>n;
+    int a[n];
+    for (int i=0;i<n;i++)
+    {
+        cin >>a[i];
+    }
+    for (int i=0;i<n;i++)
+    {
+        if (a[i]==1)
+        {
+            a1++;
+        }
+        else if (a[i]==2)
+        {
+            a2++;
+        }
+        else if (a[i]==3)
+        {
+            a3++;
+        }
+        else if (a[i]==4)
+        {
+            a4++;
+        }
+        else
+        {
+            a5++;
+        }
+    }
+    if (a1>=a2&&a1>=a3&&a1>=a4&&a1>=a5)
+    {
+        cout <<"1"<<endl;
+    }
+    else if (a2>=a1&&a2>=a3&&a2>=a4&&a2>=a5)
+    {
+        cout <<"2"<<endl;
+    }
+    else if (a3>=a1&&a3>=a2&&a3>=a4&&a3>=a5)
+    {
+        cout <<"3"<<endl;
+    }
+    else if (a4>=a1&&a4>=a2&&a4>=a3&&a4>=a5)
+    {
+        cout <<"4"<<endl;
+    }
+    else
+    {
+        cout <<"5";
+    }
+
+
+}
