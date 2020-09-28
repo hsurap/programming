ceae1aae4beb26c7545f70f88d199bf67bcbf1b7
parush2305 <parush.p2305@gmail.com>
2020-09-28 22:50:10 +0530

    very very easy

---
+++ typeCasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=7;
+    int ch1='b';
+    cout <<(a+ch1)<<endl;
+    cout <<char(a+ch1)<<endl;
+    ch1=ch1+5;
+    cout <<ch1<<endl;
+    return 0;
+}
