a93a1e6b019c5c0fcc12e12991a069346a4cfcd6
parush2305 <parush.p2305@gmail.com>
2020-10-06 14:48:48 +0530

    new concept

---
+++ cin-vs-cin,get.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    /*cin >>ch;
+    cin >>ch;
+    cin >>ch;
+    cout <<ch;*/
+    char word;
+    char u;
+    cin >>word;
+    while (word!='u')
+    {
+        cout <<word;
+        cin >>word;
+    }
+    char me;
+    me=cin.get();
+    while (me!='u')
+    {
+        cout <<me;
+        me=cin.get();
+    }
+}
