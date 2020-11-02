8ca57808aeb14f4b1ee0c82d8c2faf64bac7c869
parush2305 <parush.p2305@gmail.com>
2020-11-02 16:45:42 +0530

    easy

---
+++ apna college/binory-to-decimal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int c=0;
+    int sum=0;
+    for(int i=n;i>0;i=i/10)
+    {
+        int l=i%10;
+        sum=sum+l*pow(2,c);
+        c++;
+    }
+    cout <<sum;
+}
