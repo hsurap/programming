d88018ededfc1cfe55f597cc0f2a23b90e6385db
parush2305 <parush.p2305@gmail.com>
2021-01-05 00:25:21 +0530

    easy

---
+++ cb fundamentals/decimal-to-octal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string octal="";
+    while(n!=0){
+        int r=n%8;
+        n/=8;
+        char c=r+'0';
+        octal =c+octal;
+    }
+    cout <<octal<<endl;
+}
