d18e92c819801162280f8093d02c9559e7ad3b1b
parush2305 <parush.p2305@gmail.com>
2021-01-06 09:33:16 +0530

    dynamic

---
+++ cb fundamentals/2d-array-dynamically.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,c;
+    cin>>r>>c;
+    int **arr=new int*[r];
+    for(int i=0;i<r;i++)
+    {
+        arr[i]=new int[c];
+    }
+    int val=1;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            arr[i][j]=val;
+            cout<<arr[i][j]<<" ";
+            val++;
+        }
+        cout <<endl;
+    }
+}
