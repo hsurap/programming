f4da1fe0f2570d9d261cea8e113da6bbe4f147f1
parush2305 <parush.p2305@gmail.com>
2021-01-08 00:16:36 +0530

    easy

---
+++ cb fundamentals/wave-pattern.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[100][100];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    for(int j=0;j<m;j++)
+    {
+        if(j%2==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                cout<<a[i][j]<<" ";
+            }
+        }
+        else{
+            for(int i=n-1;i>=0;i--)
+            {
+                cout<<a[i][j]<<" ";
+            }
+        }
+    }
+}
