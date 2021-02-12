47d628e58aaac706d27f9625f7b546c45986bedc
parush2305 <parush.p2305@gmail.com>
2021-02-12 11:42:04 +0530

    declaring varible externally

---
+++ c language first semester/extern.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int i;
+increment( ) {i++; printf( "i= %d\n", i) ; }
+decrement( ){i--; printf( "i= %d\n", i) ;}
+main( ){
+printf( "\n i= %d", i) ;
+increment( ) ; increment( ) ;
+decrement( ) ; decrement( ) ;
+}
