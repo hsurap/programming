5c7a2ca89fc7f05f6595ca498147a2eb813d9ffa
parush2305 <parush.p2305@gmail.com>
2021-02-18 02:32:38 +0530

    tower of hanoi

---
+++ c language first semester/toh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void tower(int n, char A, char C, char B) {
+	if (n == 0) return;
+	tower(n-1, A, B, C);
+	printf("\n Move disk %d from rod %c to rod %c", n, A,C);
+	tower(n-1, B,C,A);
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	tower(n, 'A', 'C', 'B');
+	return 0;
+}
+
